a05c0b64acccedb2759e3c37e35b95ca0a88ca81
Luna Delrey <Luna@nekrova.com>
2018-07-28 21:03:05 +0200

    added github -> unity bridge

--- Assets/scripts/SpriteLoader.cs
+++ Assets/scripts/SpriteLoader.cs
@@ -7,6 +7,7 @@ public class SpriteLoader : MonoBehaviour {
 
     public bool root_folder_exists = false;
     public int sub_characters_found = 0;
+    public int sub_zips_found = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -22,7 +23,10 @@ public class SpriteLoader : MonoBehaviour {
             root_folder_exists = true;
         }
         string[] charactersAvailable = Directory.GetDirectories(appPath);
+        string[] zipPackages = Directory.GetFiles(appPath);
         sub_characters_found = charactersAvailable.Length;
+        sub_zips_found = zipPackages.Length;
+
 	}
 	
 	// Update is called once per frame
